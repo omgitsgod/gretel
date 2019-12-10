b5be241e7e49078cc68420bd9a777e26fc03e6c7
omgitsgod <omgitsgod@gmail.com>
2019-12-10 11:16:47 -0500

    Import Geolocation and add useEffect

--- src/useGeolocation.js
+++ src/useGeolocation.js
@@ -1,3 +1,9 @@
 import {useState, useEffect} from 'react';
+import Geolocation from '@react-native-community/geolocation';
 
-export const useGeolocation = () => {};
+export const useGeolocation = () => {
+  const [location, setLocation] = useState({});
+  const [error, setError] = useState(null);
+
+  useEffect(() => {});
+};
