ab6b4da82d6497af878fa382ad9d9cb108a26c1f
omgitsgod <omgitsgod@gmail.com>
2020-01-15 13:29:40 -0500

    Import style from the style file

--- src/Map.js
+++ src/Map.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react';
 import {Icon} from 'react-native-elements';
 import useGeolocation from './useGeolocation';
 import {mapStyle} from './constants';
+import styles from './style/Map';
 import {
   StyleSheet,
   View,
@@ -35,7 +36,7 @@ const Map = () => {
       longitudeDelta: delta,
     }),
   );
-  let icon
+  let icon;
 
   useEffect(() => {
     requestLocation();
@@ -124,42 +125,5 @@ const Map = () => {
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    ...StyleSheet.absoluteFillObject,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
-  map: {
-    ...StyleSheet.absoluteFillObject,
-  },
-  bubble: {
-    backgroundColor: 'rgba(255,255,255,0.7)',
-    paddingHorizontal: 18,
-    paddingVertical: 12,
-    borderRadius: 20,
-  },
-  latlng: {
-    width: 200,
-    alignItems: 'stretch',
-  },
-  button: {
-    width: 80,
-    paddingHorizontal: 12,
-    alignItems: 'center',
-    marginHorizontal: 10,
-  },
-  buttonContainer: {
-    flexDirection: 'row',
-    marginVertical: 20,
-    backgroundColor: 'transparent',
-  },
-  crossHair: {
-    position: 'absolute',
-    bottom: 20,
-    right: 5,
-  },
-});
 
 export default Map;
