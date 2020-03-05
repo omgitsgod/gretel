c471bbaf3e3c20448ca378b82ec8d9ffb9d538ee
omgitsgod <omgitsgod@gmail.com>
2020-03-04 22:15:00 -0500

    Import DefaultTheme for use in CustomTheme and import DarkTheme for use in Map. Format code

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import {createDrawerNavigator} from '@react-navigation/drawer';
-import {NavigationContainer} from '@react-navigation/native';
+import {NavigationContainer, DefaultTheme, DarkTheme} from '@react-navigation/native';
 import MapScreen from '../screens/MapScreen';
 
 const Drawer = createDrawerNavigator();
 
+const CustomTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: 'rgb(255, 45, 85)',
+  },
+};
+
 const AppNavigator = () => {
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={DarkTheme}>
       <Drawer.Navigator initialRouteName="Map">
-        <Drawer.Screen name="Map" component={MapScreen} />
+        <Drawer.Screen
+          name="Map"
+          component={MapScreen}
+          options={{title: 'My home'}}
+        />
       </Drawer.Navigator>
     </NavigationContainer>
   );
