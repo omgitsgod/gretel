464d18eefdc785beada580696f74c70e12d6dcfd
omgitsgod <omgitsgod@gmail.com>
2020-03-06 13:53:56 -0500

    Display Profile component inside of ProfileScreen

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {View, StyleSheet, Text} from 'react-native';
+import Profile from '../components/Profile';
 
 const ProfileScreen = ({navigation}) => {
   return (
     <View style={styles.container}>
-      <Text>ProfileScreen</Text>
+      <Profile />
     </View>
   );
 };
