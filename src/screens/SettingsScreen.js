80e3c61501def5156e955ff861188ea9f50c5207
omgitsgod <omgitsgod@gmail.com>
2020-03-06 13:52:24 -0500

    Display Settings component inside of SettingsScreen

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {View, StyleSheet, Text} from 'react-native';
+import Settings from '../components/Settings';
 
 const SettingsScreen = ({navigation}) => {
   return (
     <View style={styles.container}>
-      <Text>SettingsScreen</Text>
+      <Settings />
     </View>
   );
 };
